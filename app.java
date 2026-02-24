aa1ebaf273dfdd05616d9e62f5d01039269adbbd
Santhoshi-code <santhuhema149@gmail.com>
2026-02-24 12:20:29 +0530

    Update local code app.java

--- app.java
+++ app.java
@@ -3,4 +3,12 @@ public class HelloWorld {
         System.out.println("Hello, World!"); // Prints the string to the console
     }
 }
+//Dev-2 added the code loacal 
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x + y);
+  }
+}
 
